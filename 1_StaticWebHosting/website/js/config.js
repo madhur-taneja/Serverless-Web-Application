701ba39558301db52029a66b48d82ffb969008b2
madhur_taneja <tanejamadhur96@gmail.com>
2019-09-22 15:32:26 +0530

    Pushing the starter code

---
+++ 1_StaticWebHosting/website/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_8f6D0LI5g', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'crgrg1v40odp5tq2v4jbia2lt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
